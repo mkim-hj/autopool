a013a96cb21b8d2fb5d80e900a5b7ff45e9085a5
Rick Marron <rickmarron@Ricks-MacBook-Pro-2.local>
2015-11-13 17:02:51 -0800

    Added header and login page

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -1,15 +1,19 @@
 Router.route('/', function() {
+	this.layout("applicationLayout")
 	this.render("index");
 });
 
 Router.route('/login', function() {
+	this.layout("applicationLayout")
 	this.render("login");
 });
 
 Router.route('/maps', function() {
+	this.layout("applicationLayout")
 	this.render("maps");
 });
 
 Router.route('/matches', function() {
+	this.layout("applicationLayout")
 	this.render("matches");
 });
